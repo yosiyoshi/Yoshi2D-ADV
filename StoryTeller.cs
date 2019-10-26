9a6752a77903ce27e97bd87185494ba2b6800bff
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2019-10-27 00:20:21 +0900

    Add files via upload

--- StoryTeller.cs
+++ StoryTeller.cs
@@ -3,33 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+namespace story_test
+{
 public class StoryTeller : MonoBehaviour
 {
+    public static string[] texts;
+    public static string[] names;
+//  public static string[] images;
     public Text name;
     public Text text;
+//  public Text image;
+//  public string filename;
     public Image charaA;
     public Image bgA;
     private Sprite sprite;
-    private Sprite sprite2;
+//  private Sprite sprite2;
 
-    int x=0;
+    int lineNum=0;
 
     public void OnClick()
     {
-    if (x==0) {
-        sprite = Resources.Load<Sprite>("bluesky012");
-        sprite2 = Resources.Load<Sprite>("nadezhda");
-        bgA = bgA.GetComponent<Image>();
-        bgA.sprite = sprite;
+        TextAsset textAsset = new TextAsset();
+        textAsset = Resources.Load("story", typeof(TextAsset)) as TextAsset;
+        TextAsset textAsset2 = new TextAsset();
+        textAsset2 = Resources.Load("name", typeof(TextAsset)) as TextAsset;
+        TextAsset textAsset3 = new TextAsset();
+        textAsset3 = Resources.Load("image", typeof(TextAsset)) as TextAsset;
+        texts = textAsset.text.Split('\n');
+        names = textAsset2.text.Split('\n');
+//      images = textAsset3.text.Split('\n');
+//      filename = images[lineNum];
+//      sprite = Resources.Load<Sprite>(filename);
+        sprite = Resources.Load<Sprite>("nadezhda");
+//      sprite2 = Resources.Load<Sprite>("bluesky012");
         charaA = charaA.GetComponent<Image>();
-        charaA.sprite = sprite2;
-        name.text = "CharacterName";
-        text.text = "Text";
-	x++;
-              }
-    if (x==1) {
-        name.text = "CharacterName";
-        text.text = "Text2";
+        charaA.sprite = sprite;
+//      bgA = bgA.GetComponent<Image>();
+//      bgA.sprite = sprite2;
+        name.text = names[lineNum];
+        text.text = texts[lineNum];
+        lineNum++;
               }
     }
 }
