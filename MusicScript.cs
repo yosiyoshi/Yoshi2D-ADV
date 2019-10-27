bef671e3ca8dc03efa8724d22a786e16842370dc
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2019-10-27 13:08:17 +0900

    Add files via upload

---
+++ MusicScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour {
+    public bool DontDestroyEnabled = true;
+
+    // Use this for initialization
+    void Start () {
+        if (DontDestroyEnabled) {
+            // Keep BGM
+            DontDestroyOnLoad (this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ StoryTeller2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace story_test2
+{
+public class StoryTeller2 : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+    Debug.Log("Now scene is " + Application.loadedLevelName);
+    Application.LoadLevel("SampleScene");
+              }
+    }
+}
